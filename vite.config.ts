e0244971b54fbfdcfc126fd8de947b4b350e4107
tateno <gd28.c.tateno@gmail.com>
2024-09-16 14:45:01 +0900

    reactに最適化

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,44 @@
-import build from '@hono/vite-build/cloudflare-pages'
-import devServer from '@hono/vite-dev-server'
-import adapter from '@hono/vite-dev-server/cloudflare'
-import { defineConfig } from 'vite'
+import build from "@hono/vite-build/cloudflare-pages";
+import devServer from "@hono/vite-dev-server";
+import adapter from "@hono/vite-dev-server/cloudflare";
+import { defineConfig } from "vite";
 
-export default defineConfig({
-  plugins: [
-    build(),
-    devServer({
-      adapter,
-      entry: 'src/index.tsx'
-    })
-  ]
-})
+export default defineConfig(({ mode }) => {
+	if (mode === "client") {
+		return {
+			build: {
+				rollupOptions: {
+					input: "./src/client.tsx",
+					output: {
+						entryFileNames: "static/client.js",
+					},
+				},
+			},
+		};
+	}
+	return {
+		ssr: {
+			external: ["react", "react-dom"],
+		},
+		plugins: [
+			build(),
+			devServer({
+				adapter,
+				entry: "src/index.tsx",
+			}),
+		],
+	};
+});
+
+// export default defineConfig({
+// 	ssr: {
+// 		external: ["react", "react-dom"],
+// 	},
+// 	plugins: [
+// 		build(),
+// 		devServer({
+// 			adapter,
+// 			entry: "src/index.tsx",
+// 		}),
+// 	],
+// });
