cd93202f28caa956044926df8abe2f1531a78f1c
tateno <gd28.c.tateno@gmail.com>
2024-09-16 17:01:50 +0900

    add api response

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,4 +1,5 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
+import { useState } from "react";
 
 export const Route = createLazyFileRoute("/")({
 	component: Index,
@@ -8,6 +9,26 @@ function Index() {
 	return (
 		<div className="p-2">
 			<h3>Welcome Home!</h3>
+			<ClockButton />
 		</div>
 	);
 }
+
+const ClockButton = () => {
+	const [response, setResponse] = useState<string | null>(null);
+
+	const handleClick = async () => {
+		const response = await fetch("/api/clock");
+		const data = await response.json();
+		setResponse(JSON.stringify(data, null, 2));
+	};
+
+	return (
+		<div>
+			<button type="button" onClick={handleClick}>
+				Get Server Time
+			</button>
+			{response && <pre>{response}</pre>}
+		</div>
+	);
+};
